da52cb500be2681a5c03bb59b258e1caad70625d
mayuri <mayuri.khasale@gmail.com>
2019-06-12 15:23:39 +0530

    Change titles and styling on homepage

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 exports.createuser = (name, email, password) => {
     // TODO: save in DB
+    // eslint-disable-next-line no-console
     console.log(name, email, password);
     return { name, email };
 };
